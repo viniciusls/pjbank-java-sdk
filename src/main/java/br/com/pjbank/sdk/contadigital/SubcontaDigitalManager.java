6e069c56122942170390b673636b82b029dd3fc3
Vinícius Silva <vinicius768@msn.com>
2018-03-16 16:47:40 -0300

    Implementação de extrato para subconta

--- src/main/java/br/com/pjbank/sdk/contadigital/SubcontaDigitalManager.java
+++ src/main/java/br/com/pjbank/sdk/contadigital/SubcontaDigitalManager.java
@@ -2,24 +2,33 @@ package br.com.pjbank.sdk.contadigital;
 
 import br.com.pjbank.sdk.api.PJBankClient;
 import br.com.pjbank.sdk.auth.PJBankAuthenticatedService;
+import br.com.pjbank.sdk.enums.FormatoExtrato;
 import br.com.pjbank.sdk.enums.StatusCartaoCorporativo;
+import br.com.pjbank.sdk.enums.TipoTransacao;
 import br.com.pjbank.sdk.exceptions.PJBankException;
 import br.com.pjbank.sdk.models.common.Boleto;
 import br.com.pjbank.sdk.models.common.Endereco;
 import br.com.pjbank.sdk.models.contadigital.CartaoCorporativo;
 import br.com.pjbank.sdk.models.contadigital.Subconta;
+import br.com.pjbank.sdk.models.contadigital.TransacaoExtrato;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Vinícius Silva <vinicius.silva@superlogica.com>
@@ -147,5 +156,59 @@ public class SubcontaDigitalManager extends PJBankAuthenticatedService {
         return new Boleto(responseObject.getString("nosso_numero"),
                 responseObject.getString("link_boleto"),
                 responseObject.getString("linha_digitavel"));
+    }
+
+    /**
+     * Retorna o extrato de transações da Subconta Digital em formato JSON ou CNAB 240 [Desabilitado]
+     * @param dataInicio: Data de início do extrato desejado. Deve ser informado no formato mm/dd/aaaa.
+     * @param dataFim: Data de fim do extrato desejado. Deve ser informado no formato mm/dd/aaaa.
+     * @param formato: Formato de extrato desejado (JSON ou CNAB 240 [Desabilitado])
+     * @param pagina: Pagina o extrato. O mesmo se limita a 50 itens por página.
+     * @param comSaldo: Retorna também o saldo atual da subconta.
+     * @return List<TransacaoExtrato>
+     */
+    public List<TransacaoExtrato> extract(Date dataInicio, Date dataFim, FormatoExtrato formato, int pagina,
+                                          boolean comSaldo)
+            throws IOException, ParseException, URISyntaxException, PJBankException {
+        PJBankClient client = new PJBankClient(this.endPoint.concat("/transacoes"));
+        HttpGet httpGet = client.getHttpGetClient();
+        httpGet.addHeader("x-chave-conta", this.chave);
+
+        if (!formato.equals(FormatoExtrato.JSON))
+            httpGet.removeHeaders("Accept");
+
+        URIBuilder uriBuilder = new URIBuilder(httpGet.getURI());
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+
+        uriBuilder.addParameter("data_inicio", dateFormat.format(dataInicio));
+        uriBuilder.addParameter("data_fim", dateFormat.format(dataFim));
+        uriBuilder.addParameter("formato", formato.getName());
+        uriBuilder.addParameter("pagina", String.valueOf(pagina));
+        uriBuilder.addParameter("com_saldo", String.valueOf(comSaldo));
+
+        httpGet.setURI(uriBuilder.build());
+
+        String response = EntityUtils.toString(client.doRequest(httpGet).getEntity());
+
+        JSONArray responseArray = new JSONArray(response);
+        List<TransacaoExtrato> transacoesExtrato = new ArrayList<>();
+
+        for (int i = 0; i < responseArray.length(); i++) {
+            JSONObject responseObject = (JSONObject) responseArray.get(i);
+
+            TransacaoExtrato transacaoExtrato = new TransacaoExtrato();
+            transacaoExtrato.setIdTransacao(responseObject.getString("id_operacao"));
+            transacaoExtrato.setIdentificador(responseObject.getString("identificador"));
+            transacaoExtrato.setNomeFavorecido(responseObject.getString("nome_favorecido"));
+            transacaoExtrato.setCnpjFavorecido(responseObject.getString("cnpj_favorecido"));
+            transacaoExtrato.setDataPagamento(dateFormat.parse(responseObject.getString("data_pagamento")));
+            transacaoExtrato.setValor(responseObject.getDouble("valor"));
+            transacaoExtrato.setHistorico(responseObject.getString("historico"));
+            transacaoExtrato.setTipo(TipoTransacao.fromString(responseObject.getString("tipo_transacao")));
+
+            transacoesExtrato.add(transacaoExtrato);
+        }
+
+        return transacoesExtrato;
     }
 }
